deb8394a662672608dfb5d76c4edd52f5af64f27
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 04:09:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ monopoly = {
 	                  :reading_railroad => {},
 	                  :pennsylvania_railroad => {},
 	                  :b_and_o_railroad => {},
-	                  :shortline_railroad => {}
+	                  :shortline => {}
 	              },
 	              :rent_in_dollars =>{
 	                  :one_piece_owned => 25,
