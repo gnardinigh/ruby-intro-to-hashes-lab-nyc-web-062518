f50b3392196874d4a0f4c09e256a9f151c2f6c87
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 04:04:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,12 +13,12 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	      :railroads => {
+	          :railroads => {
 	              :pieces => 4,
-	              :names => {},
-	              :rent_in_dollars => {}
-	              }
-	      }
+	              :names =>{},
+	              :rent_in_dollars =>{}
+	          }
+	}
 	
 end
 
